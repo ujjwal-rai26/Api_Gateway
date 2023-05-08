aea6a36f68595cae953b585fa7ded39d07fea1a4
ujjwal rai <ujjwalrai26092001@gmail.com>
2023-05-08 15:56:51 +0530

    done with api gateway

---
+++ index.js
@@ -0,0 +1,53 @@
+const express=require('express');
+
+const app=express();
+
+const morgan=require('morgan');
+const axios=require('axios');
+const {createProxyMiddleware}=require('http-proxy-middleware');
+const rateLimit=require('express-rate-limit');
+
+
+PORT=3005;
+
+const limiter=rateLimit({
+    windowMs:2 * 60 * 1000,
+    max:5
+})
+
+app.use(morgan('combined'));
+app.use(limiter);
+
+app.use('/bookingservice',async (req,res,next)=>{
+
+
+    try {
+        const response=await axios.get('http://localhost:3001/api/v1/isauthenticated',{
+            headers:{
+                'x-access-token':req.headers['x-access-token']
+            }
+        })
+        console.log(response.data);
+        if(response.data.success){
+            next();
+        }
+        else{
+            return res.status(401).json({message:'unauthorised'})
+        }
+    } 
+    catch (error) {
+        return res.status(401).json({message:'unauthorised'})
+    }
+   
+})
+
+app.use('/bookingservice',createProxyMiddleware({target:'http://localhost:3002/',changeOrigin:true}));
+
+app.get('/home',(req,res)=>{
+    res.json({message:'okk'});
+})
+
+
+app.listen(PORT,()=>{
+console.log(`server started at port ${PORT}`);
+})
